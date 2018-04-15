6efe5b8121ff2670c8f7e4cb9b16054cee995434
Gabriel <g.lackermeier@gmx.de>
2018-04-15 14:16:01 +0200

    Started working on the Backend

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+require('./routes')(app)
+
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Backend started on Port ${config.port}`)
+  })
---
+++ backend/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.port || 8081,
+  db: {
+    database: process.env.DB_NAME || 'indoornav',
+    user: process.env.DB_USER || 'indoornavDBUser',
+    password: process.env.DB_PASSWORD || '',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './db.sqlite'
+    }
+  }
+}
