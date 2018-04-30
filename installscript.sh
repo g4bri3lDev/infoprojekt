c8dbc77dcc424872eae6508bf4cdfcf474fe2563
yonie1 <jonas.niesner@gmail.com>
2018-04-30 17:29:14 +0200

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -4,3 +4,10 @@ git clone https://github.com/g4bri3lDev/infoprojekt.git
 cd infoprojekt
 chmod +x startscript.sh
 ./startscript.sh
+cd frontend 
+npm install
+cd ..
+cd backend 
+npm install
+cd.. 
+echo "OK"
