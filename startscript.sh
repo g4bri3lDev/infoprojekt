888a4aae0fb01cabb2f75d85088c62ed287b7494
yonie1 <jonas.niesner@gmail.com>
2018-05-02 20:37:33 +0200

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -3,5 +3,7 @@ cd ~/Documents/infoprojekt/
 git fetch --all
 git reset --hard origin/master
 chmod +x startscript.sh
+uv4l --driver raspicam --auto-video_nr --width 1920 --height 1080 --encoding jpeg
+cd ~/mjpg-streamer/mjpg-streamer-experimental
+./mjpg_streamer -i "./input_uvc.so -rot 0" -o "./output_http.so -w ./www -p 8090" > output.log 2>&1 &
 echo "Ok"
-echo "Test"
