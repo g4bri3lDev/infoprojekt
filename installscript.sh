fb20a6a2b46116f43917dcba07c5cfe75330b255
yonie1 <jonas.niesner@gmail.com>
2018-05-03 14:12:03 +0200

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
+#sets start point
 cd ~/Documents/
+#clones the project from github
 git clone https://github.com/g4bri3lDev/infoprojekt.git
 cd infoprojekt
+#prepares the start script
 chmod +x startscript.sh
 ./startscript.sh
+#installs dep.
 cd frontend 
 npm install
 cd ..
 cd backend 
 npm install
-cd.. 
+cd .. 
+cd ..
+#installs mjpg-streamer
+sudo apt-get install cmake libjpeg8-dev
+cd mjpg-streamer-experimental
+make
+sudo make install
 echo "OK"
