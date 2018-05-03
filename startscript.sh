39df4c2cd2a3c0dd15a0a569a6e6030ae2014224
yonie1 <jonas.niesner@gmail.com>
2018-05-03 14:19:32 +0200

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cd /home/pi/Documents/infoprojekt/
+#updates the project and rearms the start script
 git fetch --all
 git reset --hard origin/master
 chmod +x startscript.sh
+#enables the camera stream
 uv4l --driver raspicam --auto-video_nr --width 1920 --height 1080 --encoding jpeg
 cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
 ./mjpg_streamer -i "./input_uvc.so -rot 0" -o "./output_http.so -w ./www -p 8090" > output.log 2>&1 &
+#starts the main programms
 cd /home/pi/Documents/infoprojekt/frontend
 npm start > output.log 2>&1 &
 cd /home/pi/Documents/infoprojekt/backend
