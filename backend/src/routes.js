798ed02f1925326666fd3b2f08c5e055e5b59abe
Gabriel <g.lackermeier@gmx.de>
2018-05-06 21:10:50 +0200

    updated TODO

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,10 @@ const GeneralController = require('./controllers/GeneralController')
 const RoomController = require('./controllers/RoomController')
 
 module.exports = (app) => {
-    app.get('/online', GeneralController.isOnline)
-    app.get('/rooms', RoomController.getRooms)
-    app.post('/addRoom', RoomController.addRoom)
-    app.post('/queryRooms', RoomController.queryRooms)
-    app.post('/deleteRoom', RoomController.deleteRoom)
+  app.get('/online', GeneralController.isOnline)
+  app.get('/rooms', RoomController.getRooms)
+  app.post('/addRoom', RoomController.addRoom)
+  app.post('/queryRooms', RoomController.queryRooms)
+  app.post('/deleteRoom', RoomController.deleteRoom)
 }
+// TODO add Functionality to Routes aswell
