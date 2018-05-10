f579d3db1b8a52e4b6d9bd65b76bbe096877c52c
yonie1 <jonas.niesner@gmail.com>
2018-05-10 15:11:40 +0200

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -6,7 +6,7 @@ git reset --hard origin/master
 chmod +x startscript.sh
 #enables the camera stream
 uv4l --driver raspicam --auto-video_nr --width 1920 --height 1080 --encoding jpeg
-cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
+cd /root/infoprojekt/mjpg-streamer/mjpg-streamer-experimental
 ./mjpg_streamer -i "./input_uvc.so -rot 0" -o "./output_http.so -w ./www -p 8090" > output.log 2>&1 &
 #starts the main programms
 cd /root/infoprojekt/frontend
