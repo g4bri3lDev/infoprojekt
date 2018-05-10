704e3348c615dceedb3dd190c6b2a0de0a808e2b
jonas <jonas.niesner@gmail.com>
2018-05-10 11:35:46 +0200

    new mc

--- backend/src/controllers/MotorController.js
+++ backend/src/controllers/MotorController.js
@@ -24,32 +24,32 @@ class Motor extends EventEmitter {
     forwards() {
         led1.pwmWrite(255);
         led2.pwmWrite(255);
-        led2.pwmWrite(0);
-        led2.pwmWrite(0);
-        led2.pwmWrite(0);
+        led3.pwmWrite(0);
+        led4.pwmWrite(0);
+        led5.pwmWrite(0);
     }
 
     backwards() {
         led1.pwmWrite(255);
         led2.pwmWrite(0);
-        led2.pwmWrite(255);
-        led2.pwmWrite(0);
-        led2.pwmWrite(0);
+        led3.pwmWrite(255);
+        led4.pwmWrite(0);
+        led5.pwmWrite(0);
     }
 
     break() {
         led1.pwmWrite(255);
         led2.pwmWrite(255);
-        led2.pwmWrite(255);
-        led2.pwmWrite(255);
-        led2.pwmWrite(255);
+        led3.pwmWrite(255);
+        led4.pwmWrite(255);
+        led5.pwmWrite(255);
     }
     off() {
         led1.pwmWrite(0);
         led2.pwmWrite(0);
-        led2.pwmWrite(0);
-        led2.pwmWrite(0);
-        led2.pwmWrite(0);
+        led3.pwmWrite(0);
+        led4.pwmWrite(0);
+        led5.pwmWrite(0);
     }
 }
 
