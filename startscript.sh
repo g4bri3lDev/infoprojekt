94b83f38df6f60f66fc51b98b762ac2240dea709
yonie1 <jonas.niesner@gmail.com>
2018-05-13 11:12:47 +0200

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -5,10 +5,9 @@ git fetch --all
 git reset --hard origin/master
 chmod +x startscript.sh
 #enables the camera stream
-uv4l --driver raspicam --auto-video_nr --width 1920 --height 1080 --encoding jpeg
-cd /root/infoprojekt/mjpg-streamer/mjpg-streamer-experimental
-./mjpg_streamer -i "./input_uvc.so -rot 180" -o "./output_http.so -w ./www -p 8090" > output.log 2>&1 &
+sudo su - pi -c "uv4l --driver raspicam --auto-video_nr --width 1920 --height 1080 --encoding jpeg"
+sudo su - pi -c '/home/pi/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -i "/home/pi/mjpg-streamer/mjpg-streamer-experimental/input_uvc.so -rot 180" -o "/home/pi/mjpg-streamer/mjpg-streamer-experimental/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer-experimental/www -p 8090" > output.log 2>&1 &'
 #starts the main programms
-npm start --prefix /root/infoprojekt/frontend > output.log 2>&1 &
-npm start --prefix /root/infoprojekt/backend > output.log 2>&1 &
+sudo su - root -c "npm start --prefix /root/infoprojekt/frontend > output.log 2>&1 &"
+sudo su - root -c "npm start --prefix /root/infoprojekt/backend > output.log 2>&1 &"
 echo "Ok"
