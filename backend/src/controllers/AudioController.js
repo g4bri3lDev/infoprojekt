3dcc56a27017ac3896af5af0885580a4bcc48c8d
Ocerry <38349447+Ocerry@users.noreply.github.com>
2018-07-05 18:32:53 +0200

    Create AudioController.js

---
+++ backend/src/controllers/AudioController.js
@@ -0,0 +1,16 @@
+const player = require('play-sound')(opts = {});
+module.exports = {
+    async play(req,res){
+
+    player.play('../../audio/Skidaddle_Skidoodle.opus', function(err){
+            if (err) throw err
+        }
+
+    )
+      res.send({ok:'yeet'})
+}
+
+
+
+
+}
