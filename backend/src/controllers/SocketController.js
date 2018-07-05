1cc9512f002d4b999c133d7e176c8b2fe7114817
yonie1 <jonas.niesner@gmail.com>
2018-07-05 18:14:01 +0200

    Update SocketController.js

--- backend/src/controllers/SocketController.js
+++ backend/src/controllers/SocketController.js
@@ -1,20 +1,30 @@
-const socketio = require('socket.io')
-const Motor = require('./mcmock');
-const motor = new Motor;
+//Socket Controller 
+//Written by Gabriel Lackermeier and Jonas Nieser  
+//Comments by Jonas Nieser
 
+const socketio = require('socket.io')
+//imports Lidat and Motor controller and creates Lidar and Motor Objects 
+const Motor = require('./mcmock');
+const Lidar = require('./LidarController');
+const motor = new Motor;
+const lidar = new Lidar;
+
+//handels the motor emitter, only for debuging 
 motor.on('test', (arg) => {
     console.log(arg);
 });
 
 module.exports = (server) => {
+    //starts the lidar
+    lidar.start();
     const io = socketio(server)
     let connections = 0
     io.on('connection', socket => {
         console.log(++connections)
-
         socket.on('disconnect', () => {
             console.log(--connections)
         })
+        //handels the the remot controll socket
         socket.on('mouseDown', (direction) => {
             //TODO if to case
             if (direction == 'back') {
@@ -29,6 +39,7 @@ module.exports = (server) => {
             if (direction == 'right') {
                 motor.right();
             }
+            //for debugging only 
             //motor.log('test');
             console.log('MouseDown Received, Direction: ' + direction)
         })
